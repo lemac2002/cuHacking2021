763cb749ae7889a45c615b2f4a897e347565f69a
Chotle <60724357+lemac2002@users.noreply.github.com>
2021-01-31 00:14:36 -0500

    Add files via upload
    QR Code scanner

---
+++ BasicTest.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+img = cv2.imread('MateiQR.PNG')
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+while True:
+  success, img = cap.read()
+  for barcode in decode(img):
+    print(barcode.data)
+    myData = barcode.data.decode('utf-8')
+    print(myData)
+    pts = np.array([barcode.polygon],np.int32)
+    pts = pts.reshape((-1,1,2))
+    cv2.polylines(img,[pts],True,(0,255,0),5)
+
+  cv2.imshow('Result',img)
+  cv2.waitKey(1)
+
+
+
+
